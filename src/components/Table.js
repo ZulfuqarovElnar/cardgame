e579166abfb27aef77f5ec070b8493c4a0a612f8
Elnar <elnarzulfuqarli2001@gmail.com>
2022-12-15 14:29:52 +0400

    En az 2 kart olacaq sekilde statik olarak hazirdir

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,27 @@
 
+
 import React from 'react';
 import  Card  from './Card';
 import './TableStyles.css';
 
-const Table = () => {
+class Table extends React.Component{
+  constructor(props) {
+    super(props);
+    this.numbers = [1,1,2,4,6]
+  }
+  
+render () {
+  const result = this.numbers.map( number => {
+    return (
+      <div className='blok col-4'><Card number={number}/></div>
+    )
+  })
   return (
     <div className='row'>
-        <div className='blok col-4'><Card /></div>
-        <div className='blok col-4'><Card /></div>
-        <div className='blok col-4'><Card /></div>
-        <div className='blok col-4'><Card /></div>
-        <div className='blok col-4'><Card /></div>
+          {result}
     </div>
   )
+}
 }
 
 export default Table
