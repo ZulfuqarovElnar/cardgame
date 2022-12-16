bc368547d8b66d7c7186e2acd2873a92d05c32a5
Elnar <elnarzulfuqarli2001@gmail.com>
2022-12-16 11:38:04 +0400

    Bir kardan en azi 2 defe olmasi

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,16 +1,38 @@
 
-
 import React from 'react';
 import  Card  from './Card';
 import './TableStyles.css';
 
+
+
 class Table extends React.Component{
   constructor(props) {
     super(props);
-    this.numbers = [1,1,2,4,6]
+    this.numbers = this.shuffle([1,1,2,4,6])
+    
   }
+
+  shuffle = function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
   
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
 render () {
+  
+  
   const result = this.numbers.map( number => {
     return (
       <div className='blok col-4'><Card number={number}/></div>
@@ -22,6 +44,7 @@ render () {
     </div>
   )
 }
+
 }
 
 export default Table
