203ac0005ab6ca00500bfca53cbcfa282ecf2074
Elnar <elnarzulfuqarli2001@gmail.com>
2022-12-17 14:17:45 +0400

    Cardlari secdikden sonra eyni olub olmasini ve neticeni gostermek

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,4 @@
+
 import React from 'react'
 import './CardStyles.css'
 
@@ -8,18 +9,23 @@ class Card extends React.Component {
       this.image = `assets/card${props.number}.jpg`
       this.handleClick = this.handleClick.bind(this);
     }
-  
+
     handleClick() {
       this.setState(prevState => ({
         isToggleOn:false // !prevState.isToggleOn
-        
-      }));
+      }
+      ));
+
+      
+
     }
-  
+    
+    
+
     render() {
       return (
-        <button onClick ={this.handleClick}>
-          {this.state.isToggleOn ? <img src="assets/card.jpg" alt="" /> : <img src={this.image} alt="" />  }
+        <button className='card-holder' onClick ={this.handleClick}>
+          {this.state.isToggleOn ? <img className='back' src="assets/card.jpg" alt="" /> : <img className = "front"  data-number={this.props.number} src={this.image} alt="" />  }
         </button>
       );
     }
