c0830eb3d7e8e810496943e92b6f73d2a97b1d60
Elnar <116576971+ZulfuqarovElnar@users.noreply.github.com>
2022-12-17 14:19:18 +0400

    Cardlari secdikden sonra eyni olub olmasini ve neticeni gostermek (#4)

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,13 +5,47 @@ import './TableStyles.css';
 
 
 
+
 class Table extends React.Component{
   constructor(props) {
     super(props);
-    this.numbers = this.shuffle(this.array())
-    
+    this.state = {gameState:"Oyuna Davam"}
+    this.numbers =  this.shuffle(this.array())
+    this.hadleGameState = this.hadleGameState.bind(this);
+    this.timeout = false;
   }
-   
+  
+  hadleGameState(){
+    
+    if (this.timeout) {
+      clearTimeout(this.timeout)
+    }
+
+    this.timeout = setTimeout(
+      ()=>{
+        // start
+        const openCards = document.querySelectorAll('.front')
+    
+          if (openCards.length === 2) {
+            const [firstCard, secondCard] = openCards
+          
+            firstCard.dataset.number === secondCard.dataset.number ?
+              this.setState(prevState => ({gameState:"Qalibsiniz"})) //Qalib gelmek
+              :
+              this.setState(prevState => ({gameState:"Meglubsunuz"}))  //Meglub olmaq
+
+            // Butun kartlari ac!
+            const cards = document.querySelectorAll('.card-holder');
+            cards.forEach((card, i) => {
+              card.click()
+            })
+
+          }
+        // end
+      }, 200
+    )
+  }
+
   array = function array(){
     const number1 =Math.floor(Math.random()*10)+1
     const number2 =Math.floor(Math.random()*10)+1
@@ -20,10 +54,7 @@ class Table extends React.Component{
     
 
     return [number1,number2,number3,number4,number1];
-  }
-
-      
-      
+  }  
       
   shuffle = function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -42,18 +73,25 @@ class Table extends React.Component{
   
     return array;
   }
-
+  
+  
 render () {
   
-  
-  const result = this.numbers.map( number => {
+  const result = this.numbers.map( (number,index) => {
     return (
-      <div className='blok col-4'><Card number={number}/></div>
-    )
-  })
+      <div key={index.toString()} onClickCapture={this.hadleGameState}  className='blok col-4'><Card number={number} />
+      </div>
+    
+    )})
+
+  const annoucement = <div className='blok col-4'>
+    { this.state.gameState }
+  </div>
   return (
     <div className='row'>
           {result}
+
+          {annoucement}
     </div>
   )
 }
